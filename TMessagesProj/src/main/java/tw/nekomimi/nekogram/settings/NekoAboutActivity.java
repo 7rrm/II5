a4d8d0adc63cc156daee2af1f5791f05705bf500
7rrm <krardh17@gmail.com>
2026-08-12 16:59:46 +0300

    Update developer and channel usernames in NekoAboutActivity

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/settings/NekoAboutActivity.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/settings/NekoAboutActivity.java
@@ -109,10 +109,10 @@ public class NekoAboutActivity extends BaseNekoSettingsActivity {
     protected void onItemClick(View view, int position, float x, float y) {
         if (position == developerRow) {
             MessagesController.getInstance(currentAccount)
-                    .openByUserName("i55544", NekoAboutActivity.this, 1);
+                    .openByUserName("Lx5x5", NekoAboutActivity.this, 1);
         } else if (position == mainChannelRow) {
             MessagesController.getInstance(currentAccount)
-                    .openByUserName("Y_VBB", NekoAboutActivity.this, 1);
+                    .openByUserName("InaRaS5", NekoAboutActivity.this, 1);
         } else if (position == channel1Row) {
             MessagesController.getInstance(currentAccount)
                     .openByUserName("nadoremalf", NekoAboutActivity.this, 1);
@@ -144,9 +144,9 @@ public class NekoAboutActivity extends BaseNekoSettingsActivity {
                 } else if (position == meeroEditionRow) {
                     textCell.setTextAndValue(MeeroStrings.s(10), String.valueOf(BuildConfig.MEERO_EDITION), true);
                 } else if (position == developerRow) {
-                    textCell.setTextAndValue(MeeroStrings.s(82), "@i55544", true);
+                    textCell.setTextAndValue(MeeroStrings.s(82), "@Lx5x5", true);
                 } else if (position == mainChannelRow) {
-                    textCell.setTextAndValue(MeeroStrings.s(463), "@Y_VBB", true);
+                    textCell.setTextAndValue(MeeroStrings.s(463), "@InaRaS5", true);
                 } else if (position == channel1Row) {
                     textCell.setTextAndValue(MeeroStrings.s(37), "@nadoremalf", true);
                 } else if (position == channel2Row) {
